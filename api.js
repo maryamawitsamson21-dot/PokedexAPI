5cc78531b31b6c8104758a8fc1b70cd2cab1736d
Maryamawit Samson <maryamawitsamson21@gmail.com>
2026-08-31 20:12:19 +0300

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ export async function api(name) {
    
     
   
-    const data= await fetch(`${import.meta.env.VITE_APIKEY}${name}`)
+    const data= await fetch(`${import.meta.env.VITE_APIKEY}${name.toLowerCase().trim()}`)
    if(!(data.ok)){
    
     return
